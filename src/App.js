54a50f4d73630003205cd5bc9475f51458a050c1
Hernan <hernanlogran@hotmail.com>
2020-01-06 19:59:15 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//pages
+import Home from "./pages/home";
+import NewMovies from "./pages/new-movies";
+import Popular from "./pages/popular";
+import Search from "./pages/search";
+import Error404 from "./pages/error404";
+import Movie from "./pages/movie";
 
 function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div>
-      <h1>Estamos en App</h1>
-    </div>
+    <Layout>
+      <Header>Header....</Header>
+      <Content>
+        <Router>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular" exact={true}>
+              <Popular />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie" exact={true}>
+              <Movie />
+            </Route>
+
+            <Route path="*">
+              <Error404 />
+            </Route>
+          </Switch>
+        </Router>
+      </Content>
+    </Layout>
   );
 }
 
--- src/pages/error404.js
+++ src/pages/error404.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Home() {
-  return "We are on home";
+export default function Error404() {
+  return "We are on Error404";
 }
