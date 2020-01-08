73649dbf80b45ccb5c1d6bbf1d6ec62886c41fff
hernan <hernanlogran@gmail.com>
2020-01-08 06:14:30 -0300

    Add component: MovieList

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import SliderMovies from "../components/SliderMovies";
+import MovieList from "../components/MovieList";
 import useFetch from "../hooks/useFetch";
+import {Col, Row} from "antd"
 import { API_URL, API_TOKEN } from "../utils/constants";
 
 export default function Home() {
   const newMovies = useFetch( `${API_URL}/3/movie/now_playing?api_key=${API_TOKEN}&language=en-US&page=1`);
+  const popularMovies = useFetch( `${API_URL}/3/movie/popular?api_key=${API_TOKEN}&language=en-US&page=1`);
+  const topRatedMovies = useFetch( `${API_URL}/3/movie/top_rated?api_key=${API_TOKEN}&language=en-US&page=1`);
   return (
-    <SliderMovies movies={newMovies}/>
+    <div>
+      <SliderMovies movies={newMovies}/>
+      <Row>
+        <Col span={12}><MovieList movies={popularMovies} title="Popular Movies"/></Col>
+        <Col span={12}><MovieList movies={topRatedMovies} title="Top Rated Movies"/></Col>
+      </Row>
+    </div>
   );
 }
