22af03e80b93cb92e4a151d150e69897c1c6aa7b
Hernan <hernanlogran@hotmail.com>
2020-01-11 18:55:59 -0300

    Updates

---
+++ precache-manifest.0f40fcf57a59dfca6bd46244a536136a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57de75b04d88a50aaa156b0a60495c2f",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "ec17bfb0bd0723326eff",
+    "url": "/react-movies/static/css/2.e890f010.chunk.css"
+  },
+  {
+    "revision": "9d601df729273f8c0170",
+    "url": "/react-movies/static/css/main.8f4dd723.chunk.css"
+  },
+  {
+    "revision": "ec17bfb0bd0723326eff",
+    "url": "/react-movies/static/js/2.aec4a6e5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-movies/static/js/2.aec4a6e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d601df729273f8c0170",
+    "url": "/react-movies/static/js/main.40f58b87.chunk.js"
+  },
+  {
+    "revision": "0099f3dc84f11bd86650",
+    "url": "/react-movies/static/js/runtime-main.412772dc.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/react-movies/static/media/logo.cb90c5bd.svg"
+  }
+]);
